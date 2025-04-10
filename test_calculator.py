a1083f6614e2a651c43c762f3be999afa71a8cff
KatieGrays <katiegray@ufl.edu>
2025-04-10 16:39:30 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,70 +1,66 @@
-import unittest
-from calculator import *
+# https://github.com/KatieGrays/lab10-KG-AE.git
+# Katie Gray & Ariella Efraim
+
 import unittest
 import calculator
 
+
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    def test_add(self): # 3 assertions
-    #     fill in code
-    #     add().__init__(a, b)
+    def test_add(self):
         self.assertEqual(calculator.add(1, 2), 3)
+        self.assertEqual(calculator.add(-1, -1), -2)
+        self.assertEqual(calculator.add(0, 0), 0)
 
-
-    def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
-    #     sub().__init__(a, b)
-        self.assertEqual(calculator.sub(2,1), 1)
+    def test_subtract(self):
+        self.assertEqual(calculator.sub(2, 1), 1)
+        self.assertEqual(calculator.sub(-1, -1), 0)
+        self.assertEqual(calculator.sub(0, 0), 0)
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):
+        self.assertEqual(calculator.mul(2, 3), 6)
+        self.assertEqual(calculator.mul(-2, 3), -6)
+        self.assertEqual(calculator.mul(0, 3), 0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self):
+        self.assertEqual(calculator.div(3, 2), 1.5)
+        self.assertEqual(calculator.div(-3, 2), -1.5)
+        self.assertEqual(calculator.div(0, 2), 0)
 
     ######## Partner 2
-    def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            calculator.div(0,5)
+            calculator.div(5, 0)
 
-    def test_logarithm(self): # 3 assertions
-    #     fill in code
-    #     log().__init__(a, b)
-        self.assertEqual(calculator.log(2, 8), 3)
+    def test_logarithm(self):
+        self.assertAlmostEqual(calculator.logarithm(8, 2), 3)
+        self.assertAlmostEqual(calculator.logarithm(16, 2), 4)
+        self.assertAlmostEqual(calculator.logarithm(25, 5), 2)
 
-
-    def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            calculator.log(-10,-100)
-    
+            calculator.logarithm(-10, -100)
+
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            calculator.logarithm(0, 5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(calculator.hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(calculator.hypotenuse(5, 12), 13)
+        self.assertAlmostEqual(calculator.hypotenuse(0, 0), 0)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
+    def test_sqrt(self):
+        self.assertAlmostEqual(calculator.square_root(9), 3.0)
+        self.assertAlmostEqual(calculator.square_root(16), 4.0)
+        self.assertIsNone(calculator.square_root(-4))
+
     #     # Test basic function
-    #     fill in code
-    ##########################
+        #     fill in code
+        ##########################
 
-# Do not touch this
-if __name__ == "__main__":
-    unittest.main()
+        # Do not touch this
+
+    if __name__ == "__main__":
+        unittest.main()
