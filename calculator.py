e1b578291d605d8d0de96bbd8599fda06858c71e
Tyrell Wellick <katiegray@ufl.edu>
2025-04-10 12:12:00 -0400

    hello

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +6,24 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
+def add(a, b):
+    return a + b
     pass
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return b / a # raise ZeroDivisionError if a == 0
+
+def log(a, b):
+    return math.log(a,b)
+    # use math library + raise ValueError
+
+def exp(a, b):
+    return a**b
 
 
